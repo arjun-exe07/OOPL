e66cdce7cc2afbc4bfa824c694208c27c0b8b784
arjun-exe07 <arjunkadam202007@gmail.com>
2026-08-20 16:49:45 +0530

    Added book.cpp

---
+++ books.cpp
@@ -0,0 +1,31 @@
+/*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Book
+{
+private:
+  char *name, *author;
+
+public:
+  Book();
+
+  ~Book();
+};
+
+int main()
+{
+
+  return 0;
+}
+
+Book::Book()
+{
+}
+
+Book::~Book()
+{
+}
